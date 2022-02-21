274e30769a6ff5d5739d21eec4e698bc29f2fd95
rentkent <rentkent29@gmail.com>
2022-02-21 15:21:20 +0530

    first commit

---
+++ ios/PhyConnectModule.swift
@@ -0,0 +1,44 @@
+//
+//  PhyConnectModule.swift
+//  PhyConnectModule
+//
+//  Copyright © 2022 Pankaj Patel. All rights reserved.
+//
+
+
+import Foundation
+import PhylloConnect
+
+public var phylloConnectInstance = PhylloManager()  //DeviceManager.swift instance
+var nativeMethodInstance = PhyConnectModule()
+
+
+@objc(PhyConnectModule)
+public class PhyConnectModule: RCTEventEmitter {
+  
+  override public func supportedEvents() -> [String]! {
+    //I will be honest, I am sending these events from DeviceManager.swift, but react native's packager gripes if I dont put the events that DeviceManager.swift is sending through the rootView's bridge here
+    return ["onAccountConnected","onAccountDisconnected","onTokenExpired","onExit"]
+  }
+  
+  public override init() {
+    super.init()
+  }
+  
+ 
+  @objc
+  public override func constantsToExport() -> [AnyHashable : Any]! {
+    return ["count": 1]
+  }
+
+  @objc(initialize:::::)
+  func initialize(clientDisplayName:String,token:String,userId:String ,environment:String, workPlatformId:String) {
+    debugPrint("start initialize ..........")
+    phylloConnectInstance.initialize(clientDisplayName: clientDisplayName, token: token, userId: userId, environment: environment, workPlatformId: workPlatformId)
+  }
+    
+  @objc public override static func requiresMainQueueSetup() -> Bool {
+      return false
+  }
+}
+
---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import PhylloConnect from './PhylloConnect'
+
+export default PhylloConnect
+// "compile": "rm -rf lib && tsc -p .",
+// "lint": "eslint . --ext .js,.jsx,.ts,.tsx",
+// "prepare": "yarn compile",
